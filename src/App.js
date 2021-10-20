897634cc3a039796fad12a8aff8ef41674ccb185
Ivan Saez <ivansotosaez69@gmail.com>
2021-10-20 19:49:30 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import React, { Fragment, useState, useEffect } from 'react';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+import Clima from './components/Clima';
+import Error from './components/Error';
+
+function App() {
+
+  // state de formulario
+  const [ busqueda, guardarBusqueda ] = useState({
+      ciudad: '',
+      pais: ''
+  });
+
+  const [ consultar, guardarConsulta ] = useState(false);
+
+  const [ resultado, guardarResultado ] = useState({});
+
+  const [ error, guardarError ] = useState(false);
+
+  const { ciudad, pais } = busqueda;
+
+
+  useEffect(()=>{
+    const consultarApi = async () => {
+      if(consultar){
+        const appId = 'c73949488a2bc7ed84ad4b0853163159';
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+        guardarResultado(resultado);
+        guardarConsulta(false);
+        //detecta si hay resultados correctos en la consulta
+        if(resultado.cod === "404"){
+          guardarError(true);
+        }else{
+          guardarError(false);
+        }
+      }
+    }
+    consultarApi();
+    //eslint-disable-next-line
+  },[consultar]);
+
+  let componente;
+
+  if (error) {
+    componente = <Error mensaje="No hay resuldatos"/>
+  }else{
+    componente = <Clima
+                    resultado={resultado}
+                  />
+  }
+  
+
+  return (
+    <Fragment>
+      <Header
+        titulo='Clima React App'
+      />
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col m6 s12">
+              <Formulario
+                busqueda={busqueda}
+                guardarBusqueda={guardarBusqueda}
+                guardarConsulta={guardarConsulta}
+              />
+            </div>
+            <div className="col m6 s12">
+              {componente}
+            </div>
+          </div>
+        </div> 
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
